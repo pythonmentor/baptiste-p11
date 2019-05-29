820f31d642933be15d96f10d4c9b6516543b9a77
baptiste <baptiste.name>
2019-05-29 17:48:58 +0200

    structure projet, premier push

---
+++ maze/models/node.py
@@ -0,0 +1,8 @@
+
+class Node():
+	def __init__(self):
+		self.data = None
+		self.right = None
+		self.left = None
+		self.up = None
+		self.down = None
---
+++ tests/unit/test_node.py
@@ -0,0 +1,4 @@
+from maze.models.node import Node
+
+print('toto')
+node = Node()
